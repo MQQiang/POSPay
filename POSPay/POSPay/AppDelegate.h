aabe3f56740e08fe8aa6d61eab5a5f44f456f3b5
dead__pl <mengqiang.q@gmail.com>
2015-01-02 05:01:23 +0800

    Init Commit

---
+++ POSPay/POSPay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  POSPay
+//
+//  Created by mq on 15/1/2.
+//  Copyright (c) 2015年 mqq.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ POSPay/POSPay/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  POSPay
+//
+//  Created by mq on 15/1/2.
+//  Copyright (c) 2015年 mqq.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
