26da57c45dbcac57779c1f81e07d6c5314b04056
dead__pl <mengqiang.q@gmail.com>
2015-01-16 23:32:20 +0800

    add user info class

---
+++ POSPay/POSPay/UserInfo.h
@@ -0,0 +1,13 @@
+//
+//  UserInfo.h
+//  POSPay
+//
+//  Created by mq on 15/1/16.
+//  Copyright (c) 2015年 mqq.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserInfo : NSObject
+
+@end
---
+++ POSPay/POSPay/UserInfo.m
@@ -0,0 +1,13 @@
+//
+//  UserInfo.m
+//  POSPay
+//
+//  Created by mq on 15/1/16.
+//  Copyright (c) 2015年 mqq.com. All rights reserved.
+//
+
+#import "UserInfo.h"
+
+@implementation UserInfo
+
+@end
