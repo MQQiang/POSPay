3f71b7290c22e09ba71b4e64fe3f9deba9fa274c
leon <773004798@qq.com>
2015-02-05 13:11:19 +0800

    实名认证增加照片功能添加
    添加照片

--- POSPay/POSPay/pictureController.m
+++ POSPay/POSPay/pictureController.m
@@ -8,14 +8,14 @@
 
 #import "pictureController.h"
 
-@interface pictureController ()
+@interface pictureController ()<UIActionSheetDelegate, UIImagePickerControllerDelegate>
 @property (weak, nonatomic) IBOutlet UIButton *mainPicture;
 @property (weak, nonatomic) IBOutlet UIButton *frontPicture;
 @property (weak, nonatomic) IBOutlet UIButton *backPicture;
+@property (weak, nonatomic) UIButton *currentBtn;
 
-- (IBAction)addMainPicture;
-- (IBAction)addFrontPicture;
-- (IBAction)addBackPicture;
+- (IBAction)addPicture:(id)sender;
+
 
 - (IBAction)upload;
 
@@ -26,6 +26,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self addBackBtn];
+    //示例按钮
+    [self addRightBtn];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -43,16 +45,46 @@
 }
 */
 
-- (IBAction)addMainPicture {
+
+
+- (IBAction)addPicture:(id)sender {
+    UIActionSheet *sheet;
+    self.currentBtn = sender;
+    
+    // 判断是否支持相机
+    
+    if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera])
+        
+    {
+        sheet  = [[UIActionSheet alloc] initWithTitle:@"选择" delegate:self cancelButtonTitle:nil destructiveButtonTitle:@"取消" otherButtonTitles:@"拍照",@"从相册选择", nil];
+        
+    }
+    
+    else {
+        
+        sheet = [[UIActionSheet alloc] initWithTitle:@"选择" delegate:self cancelButtonTitle:nil destructiveButtonTitle:@"取消" otherButtonTitles:@"从相册选择", nil];
+        
+    }
+    
+    sheet.tag = 255;
+    
+    [sheet showInView:self.view];
+
 }
 
-- (IBAction)addFrontPicture {
-}
 
-- (IBAction)addBackPicture {
-}
 
 - (IBAction)upload {
+}
+- (void)addRightBtn{
+    UIButton *exampleBtn = [[UIButton alloc]initWithFrame:CGRectMake(0, 0, 44, 44)];
+    [exampleBtn setTitle:@" 示例" forState:UIControlStateNormal];
+    exampleBtn.titleLabel.font = [UIFont systemFontOfSize:12.0];
+    [exampleBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    //[exampleBtn addTarget:self action:<#(SEL)#> forControlEvents:UIControlEventTouchUpInside];
+    UIBarButtonItem *exampleItem = [[UIBarButtonItem alloc]initWithCustomView:exampleBtn];
+    self.navigationItem.rightBarButtonItem = exampleItem;
+    
 }
 //添加返回按钮
 - (void)addBackBtn{
@@ -64,5 +96,74 @@
 }
 - (void)backward{
     [self.navigationController popViewControllerAnimated:YES];
+}
+#pragma -mark actionsheet delegate
+-(void) actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (actionSheet.tag == 255) {
+        
+        NSUInteger sourceType = 0;
+        
+        // 判断是否支持相机
+        if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+            
+            switch (buttonIndex) {
+                case 0:
+                    // 取消
+                    return;
+                case 1:
+                    // 相机
+                    sourceType = UIImagePickerControllerSourceTypeCamera;
+                    break;
+                    
+                case 2:
+                    // 相册
+                    sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+                    break;
+            }
+        }
+        else {
+            if (buttonIndex == 0) {
+                
+                return;
+            } else {
+                sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+            }
+        }
+        // 跳转到相机或相册页面
+        UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+        
+        imagePickerController.delegate = self;
+        
+        //imagePickerController.allowsEditing = YES;
+        
+        imagePickerController.sourceType = sourceType;
+        
+        [self presentViewController:imagePickerController animated:YES completion:^{}];
+        
+        
+    }
+}
+#pragma mark - image picker delegte
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    [picker dismissViewControllerAnimated:YES completion:^{}];
+    
+    UIImage *image = [info objectForKey:UIImagePickerControllerOriginalImage];
+    /* 此处info 有六个值
+     * UIImagePickerControllerMediaType; // an NSString UTTypeImage)
+     * UIImagePickerControllerOriginalImage;  // a UIImage 原始图片
+     * UIImagePickerControllerEditedImage;    // a UIImage 裁剪后图片
+     * UIImagePickerControllerCropRect;       // an NSValue (CGRect)
+     * UIImagePickerControllerMediaURL;       // an NSURL
+     * UIImagePickerControllerReferenceURL    // an NSURL that references an asset in the AssetsLibrary framework
+     * UIImagePickerControllerMediaMetadata    // an NSDictionary containing metadata from a captured photo
+     */
+    
+    [self.currentBtn setImage:nil forState:UIControlStateNormal];
+    [self.currentBtn setBackgroundImage:image forState:UIControlStateNormal];
+    
+    self.currentBtn.tag = 100;
+    
 }
 @end
