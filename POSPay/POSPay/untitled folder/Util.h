81fa554c68e1a01def28731929e11f9bcaf6f351
dead__pl <mengqiang.q@gmail.com>
2015-02-09 00:32:40 +0800

    add des encode

--- POSPay/POSPay/untitled folder/Util.h
+++ POSPay/POSPay/untitled folder/Util.h
@@ -25,4 +25,27 @@
 +(void)alertNetworkError:(UIView *)view;
 +(NSString *)passwordStringInMD5:(NSString *)pw;
 +(BOOL)inputIsNull:(UITextField *)textField;
+
++ (NSString *)encryptWithText:(NSString *)sText;//加密
++ (NSString *)decryptWithText:(NSString *)sText;//解密
+
+
+// 把一个byte数据转换为字符串
++(NSString *) parseByte2HexString:(Byte *) bytes;
+// 把一个byte数组转换为字符串
++(NSString *) parseByteArray2HexString:(Byte[]) bytes;
+
+
+
+// nsData 转16进制
++ (NSString*)stringWithHexBytes2:(NSData *)sender;
+
+
+
+/****** 加密 ******/
++(NSString *) encryptUseDES:(NSString *)clearText key:(NSString *)key;
+/****** 解密 ******/
++(NSString *) decryptUseDES:(NSString *)plainText key:(NSString *)key;
+
+
 @end
