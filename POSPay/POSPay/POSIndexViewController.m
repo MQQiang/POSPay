cfb6d91ba21518cfe51985a51590915f14d2b1a8
leon <773004798@qq.com>
2015-03-26 22:31:11 +0800

    首页添加所有按钮

--- POSPay/POSPay/POSIndexViewController.m
+++ POSPay/POSPay/POSIndexViewController.m
@@ -15,6 +15,8 @@
 //图片轮播器
 @property (weak, nonatomic) IBOutlet UIScrollView *adView;
 //应用格子
+@property (weak, nonatomic) IBOutlet UIScrollView *applicationScrollView;
+
 - (IBAction)scanPayment:(id)sender;
 @property (strong, nonatomic) NSTimer *timer;
 @end
@@ -26,6 +28,8 @@
     self.adView.showsVerticalScrollIndicator = NO;
     //v self.adView.contentOffset = CGPointMake(0, 0);
     [self addAdView];
+    
+    [self addAppViewBtn];
     
     
 }
@@ -103,6 +107,121 @@
     if (scrollView == self.adView) {
     [self addTimer];
     }
+}
+- (void)addAppViewBtn{
+    self.applicationScrollView.pagingEnabled = YES;
+    self.applicationScrollView.contentSize = CGSizeMake(1280, 210);
+    self.applicationScrollView.scrollIndicatorInsets = UIEdgeInsetsMake(0, 0, 300, 0);
+    //第1页
+    //刷卡支付
+    UIButton *pushCardPayBtn = [[UIButton alloc]initWithFrame:CGRectMake(5, 5, 150, 100)];
+    [pushCardPayBtn setBackgroundImage:[UIImage imageNamed:@"按钮1"] forState:UIControlStateNormal];
+    [pushCardPayBtn addTarget:self action:@selector(pushCardPayment:) forControlEvents:UIControlEventTouchUpInside];
+    [self.applicationScrollView addSubview: pushCardPayBtn];
+    //无卡支付
+    UIButton *pushNoCardPayBtn = [[UIButton alloc]initWithFrame:CGRectMake(165, 5, 150, 100)];
+    [pushNoCardPayBtn setBackgroundImage:[UIImage imageNamed:@"按钮2"] forState:UIControlStateNormal];
+    [pushNoCardPayBtn addTarget:self action:@selector(pushNoCardPayment:) forControlEvents:UIControlEventTouchUpInside];
+    [self.applicationScrollView addSubview: pushNoCardPayBtn];
+    //NFC支付
+    UIButton *NFCPayBtn = [[UIButton alloc]initWithFrame:CGRectMake(5, 110, 100, 100)];
+    [NFCPayBtn setBackgroundImage:[UIImage imageNamed:@"按钮8"] forState:UIControlStateNormal];
+    //[pushCardPayBtn addTarget:self action:@selector(pushCardPayment:) forControlEvents:UIControlEventTouchUpInside];
+    [self.applicationScrollView addSubview: NFCPayBtn];
+    //扫描支付
+    UIButton *scanPayBtn = [[UIButton alloc]initWithFrame:CGRectMake(110, 110, 100, 100)];
+    [scanPayBtn setBackgroundImage:[UIImage imageNamed:@"按钮9"] forState:UIControlStateNormal];
+    [scanPayBtn addTarget:self action:@selector(scanPayment:) forControlEvents:UIControlEventTouchUpInside];
+    [self.applicationScrollView addSubview: scanPayBtn];
+    //会员积分支付
+    UIButton *jifenPayBtn = [[UIButton alloc]initWithFrame:CGRectMake(215, 110, 100, 100)];
+    [jifenPayBtn setBackgroundImage:[UIImage imageNamed:@"按钮10"] forState:UIControlStateNormal];
+    //[pushCardPayBtn addTarget:self action:@selector(pushCardPayment:) forControlEvents:UIControlEventTouchUpInside];
+    [self.applicationScrollView addSubview: jifenPayBtn];
+    //广告（主页按钮）
+    UIButton *adBtn = [[UIButton alloc]initWithFrame:CGRectMake(5, 215, 310, 70)];
+    [adBtn setBackgroundImage:[UIImage imageNamed:@"主页按钮"] forState:UIControlStateNormal];
+    //[adBtn addTarget:self action:<#(SEL)#> forControlEvents:UIControlEventTouchUpInside]
+    [self.applicationScrollView addSubview:adBtn];
+    //第2页
+    //话费
+    UIButton *huafeiBtn = [[UIButton alloc]initWithFrame:CGRectMake(325, 5, 100, 100)];
+    [huafeiBtn setBackgroundImage:[UIImage imageNamed:@"按钮11"] forState:UIControlStateNormal];
+    //[huafeiBtn addTarget:self action:@selector(pushCardPayment:) forControlEvents:UIControlEventTouchUpInside];
+    [self.applicationScrollView addSubview: huafeiBtn];
+    //水电
+    UIButton *shuidianBtn = [[UIButton alloc]initWithFrame:CGRectMake(430, 5, 100, 100)];
+    [shuidianBtn setBackgroundImage:[UIImage imageNamed:@"按钮12"] forState:UIControlStateNormal];
+    //[huafeiBtn addTarget:self action:@selector(pushCardPayment:) forControlEvents:UIControlEventTouchUpInside];
+    [self.applicationScrollView addSubview: shuidianBtn];
+    //点卡
+    UIButton *diankaBtn = [[UIButton alloc]initWithFrame:CGRectMake(535, 5, 100, 100)];
+    [diankaBtn setTitle:@"游戏点卡" forState:UIControlStateNormal];
+    //[diankaBtn setBackgroundImage:[UIImage imageNamed:@"按钮1"] forState:UIControlStateNormal];
+    //[huafeiBtn addTarget:self action:@selector(pushCardPayment:) forControlEvents:UIControlEventTouchUpInside];
+    [self.applicationScrollView addSubview: diankaBtn];
+    //交通罚款
+    UIButton *jiaotongBtn = [[UIButton alloc]initWithFrame:CGRectMake(325, 110, 100, 100)];
+    [jiaotongBtn setBackgroundImage:[UIImage imageNamed:@"18"] forState:UIControlStateNormal];
+    //[huafeiBtn addTarget:self action:@selector(pushCardPayment:) forControlEvents:UIControlEventTouchUpInside];
+    [self.applicationScrollView addSubview: jiaotongBtn];
+    //QB充值
+    UIButton *QBBtn = [[UIButton alloc]initWithFrame:CGRectMake(430, 110, 100, 100)];
+    [QBBtn setBackgroundImage:[UIImage imageNamed:@"按钮13"] forState:UIControlStateNormal];
+    //[huafeiBtn addTarget:self action:@selector(pushCardPayment:) forControlEvents:UIControlEventTouchUpInside];
+    [self.applicationScrollView addSubview: QBBtn];
+    //彩票
+    UIButton *caipiaoBtn = [[UIButton alloc]initWithFrame:CGRectMake(535, 110, 100, 100)];
+    [caipiaoBtn setBackgroundImage:[UIImage imageNamed:@"15"] forState:UIControlStateNormal];
+    //[huafeiBtn addTarget:self action:@selector(pushCardPayment:) forControlEvents:UIControlEventTouchUpInside];
+    [self.applicationScrollView addSubview: caipiaoBtn];
+    //加油卡
+    UIButton *jiayoukaBtn = [[UIButton alloc]initWithFrame:CGRectMake(325, 215, 100, 100)];
+    [jiayoukaBtn setBackgroundImage:[UIImage imageNamed:@"17"] forState:UIControlStateNormal];
+    //[huafeiBtn addTarget:self action:@selector(pushCardPayment:) forControlEvents:UIControlEventTouchUpInside];
+    [self.applicationScrollView addSubview: jiayoukaBtn];
+    //电影票
+    UIButton *dianyingpiaoBtn = [[UIButton alloc]initWithFrame:CGRectMake(430, 215, 100, 100)];
+    [dianyingpiaoBtn setBackgroundImage:[UIImage imageNamed:@"16"] forState:UIControlStateNormal];
+    //[huafeiBtn addTarget:self action:@selector(pushCardPayment:) forControlEvents:UIControlEventTouchUpInside];
+    [self.applicationScrollView addSubview: dianyingpiaoBtn];
+    //支付宝充值
+    UIButton *zhifubaoBtn = [[UIButton alloc]initWithFrame:CGRectMake(535, 215, 100, 100)];
+    [zhifubaoBtn setBackgroundImage:[UIImage imageNamed:@"14"] forState:UIControlStateNormal];
+    //[huafeiBtn addTarget:self action:@selector(pushCardPayment:) forControlEvents:UIControlEventTouchUpInside];
+    [self.applicationScrollView addSubview: zhifubaoBtn];
+    //第3页
+    //酒店预订
+    UIButton *hotelBtn = [[UIButton alloc]initWithFrame:CGRectMake(645, 5, 150, 100)];
+    [hotelBtn setBackgroundImage:[UIImage imageNamed:@"按钮3"] forState:UIControlStateNormal];
+    //[huafeiBtn addTarget:self action:@selector(pushCardPayment:) forControlEvents:UIControlEventTouchUpInside];
+    [self.applicationScrollView addSubview: hotelBtn];
+    //景点门票
+    UIButton *jingdianmenpiaoBtn = [[UIButton alloc]initWithFrame:CGRectMake(805, 5, 150, 100)];
+    [jingdianmenpiaoBtn setBackgroundImage:[UIImage imageNamed:@"按钮4"] forState:UIControlStateNormal];
+    //[huafeiBtn addTarget:self action:@selector(pushCardPayment:) forControlEvents:UIControlEventTouchUpInside];
+    [self.applicationScrollView addSubview: jingdianmenpiaoBtn];
+    //飞机票
+    UIButton *feijipiaoBtn = [[UIButton alloc]initWithFrame:CGRectMake(645, 110, 100, 100)];
+    [feijipiaoBtn setBackgroundImage:[UIImage imageNamed:@"按钮5"] forState:UIControlStateNormal];
+    //[huafeiBtn addTarget:self action:@selector(pushCardPayment:) forControlEvents:UIControlEventTouchUpInside];
+    [self.applicationScrollView addSubview: feijipiaoBtn];
+    //火车票
+    UIButton *huochepiaoBtn = [[UIButton alloc]initWithFrame:CGRectMake(750, 110, 100, 100)];
+    [huochepiaoBtn setBackgroundImage:[UIImage imageNamed:@"按钮6"] forState:UIControlStateNormal];
+    //[huafeiBtn addTarget:self action:@selector(pushCardPayment:) forControlEvents:UIControlEventTouchUpInside];
+    [self.applicationScrollView addSubview: huochepiaoBtn];
+    //汽车票
+    UIButton *qichepiaoBtn = [[UIButton alloc]initWithFrame:CGRectMake(855, 110, 100, 100)];
+    [qichepiaoBtn setBackgroundImage:[UIImage imageNamed:@"按钮7"] forState:UIControlStateNormal];
+    //[huafeiBtn addTarget:self action:@selector(pushCardPayment:) forControlEvents:UIControlEventTouchUpInside];
+    [self.applicationScrollView addSubview:qichepiaoBtn];
+    //第4页
+    //微信商城
+    UIButton *weixinshangchengBtn = [[UIButton alloc]initWithFrame:CGRectMake(965, 5, 310 , 70)];
+    [weixinshangchengBtn setBackgroundImage:[UIImage imageNamed:@"微信商城按钮"] forState:UIControlStateNormal];
+    //[huafeiBtn addTarget:self action:@selector(pushCardPayment:) forControlEvents:UIControlEventTouchUpInside];
+    [self.applicationScrollView addSubview: weixinshangchengBtn];
 }
 
 - (IBAction)pushCardPayment:(id)sender {
